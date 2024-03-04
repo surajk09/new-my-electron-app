17ec735bdf1118d9315913f849927d0de33eae6c
surajk09 <surajkhadekar@yahoo.com>
2024-03-04 15:43:24 +0530

    initial

---
+++ main.js
@@ -0,0 +1,71 @@
+const { app, BrowserWindow, ipcMain,screen,Menu} = require('electron')
+const path = require('node:path')
+const os = require('os');
+
+const createWindow = () => {
+  const {width,height} = screen.getPrimaryDisplay().workAreaSize;
+  const win = new BrowserWindow({
+    frame: true,
+    width: 300,
+    height: 400,
+    useContentSize: true,
+    transparent: false,
+    resizable: false,
+    x: width - 300,
+    y: height - 450,
+    visibleOnAllWorkspaces: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  //Menu.setApplicationMenu(null);
+  screen.on('display-metrics-changed', (event, display, changedMetrics) =>
+  {
+    // console.log(display, changedMetrics);
+    const {x, y, width, height} = display.workArea;
+    // console.log(x, y, width, height);
+    win.setBounds({x: width - 300, y: height - 450, width: 300, height: 400})
+  });
+
+  win.loadFile('index.html')
+
+  win.webContents.on('did-finish-load', () => {
+    win.webContents.send('deviceInfo', {
+      platform: os.platform(),
+      arch: os.arch(),
+      cpus: os.cpus(),
+      totalMemory: os.totalmem(),
+      freeMemory: os.freemem(),
+      appVersion: app.getVersion()
+    });
+  });
+}
+
+//for mac doc
+const dockMenu = Menu.buildFromTemplate([
+  {
+    label: 'New Window',
+    click () { console.log('New Window') }
+  }, {
+    label: 'New Window with Settings',
+    submenu: [
+      { label: 'Basic' },
+      { label: 'Pro' }
+    ]
+  },
+  { label: 'New Command...' }
+])
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  if (process.platform === 'darwin') {
+    app.dock.setMenu(dockMenu)
+  }
+  
+}).then(createWindow)
+
+ipcMain.on('close', () => {
+  app.quit()
+})
---
+++ renderer.js
@@ -0,0 +1,20 @@
+
+const func = async () => {
+    const response = await window.versions.ping()
+    console.log(response) // prints out 'pong'
+  }
+
+  const NOTIFICATION_TITLE = 'Title'
+  const NOTIFICATION_BODY = 'Notification from the Electron. Click to log to console.'
+  const CLICK_MESSAGE = 'Notification clicked!'
+  
+  new window.Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
+    .onclick = () => { document.getElementById('output').innerText = CLICK_MESSAGE }
+// // close app
+// function closeApp(e) {
+//   e.preventDefault()
+//   ipc.send('close')
+// }
+
+// document.getElementById("closeBtn").addEventListener("click", closeApp);
+  func()
